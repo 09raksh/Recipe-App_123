9045f92e647ac34b5bc35d1f5cec5068d8d46695
09raksh <rakshithavasanthrajs@gmail.com>
2025-04-08 10:45:14 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://rakshithavasanthrajs:6uL2px89LLbHbzEJ@cluster0.uskau0t.mongodb.net/rashh_db?retryWrites=true&w=majority&appName=Cluster0"
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
+
+app.listen(3001, () => console.log("Server started"));
